254ed09449bd7a603de93e30373f7c18975ecddb
alainlo1 <75679323+inestellagreen@users.noreply.github.com>
2021-11-29 15:49:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+      var hoverDistort = new hoverEffect({
+            parent: document.querySelector('.wrapper'),
+            intensity: 0.2,
+            image1: 'fille 3.png',
+            image2: 'header.png',
+            displacementImage: '7.jpg'
+      });
+
